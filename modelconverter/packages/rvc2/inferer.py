0065c4759f2d07b80d18dfafa31cb7acbc641399
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-08-05 23:42:47 +0200

    updated inferer

--- modelconverter/packages/rvc2/inferer.py
+++ modelconverter/packages/rvc2/inferer.py
@@ -14,8 +14,8 @@ class RVC2Inferer(Inferer):
         self.xml_path = self.model_path
         self.bin_path = self.model_path.with_suffix(".bin")
         ie = Core()
-        net = ie.read_network(model=self.xml_path, weights=self.bin_path)
-        self.exec_net = ie.load_network(network=net, device_name="CPU")
+        model = ie.compile_model(model=self.xml_path, device_name="CPU")
+        self.infer_request = model.create_infer_request()
 
     def infer(self, inputs: Dict[str, Path]) -> Dict[str, np.ndarray]:
         arr_inputs = {
@@ -25,7 +25,11 @@ class RVC2Inferer(Inferer):
                 encoding=self.encoding[name],
                 resize_method=self.resize_method[name],
                 data_type=self.in_dtypes[name],
-            )
+            )[np.newaxis, ...]
             for name, path in inputs.items()
         }
-        return self.exec_net.infer(inputs=arr_inputs)
+        result = self.infer_request.infer(inputs=arr_inputs)
+        return {
+            next(iter(name)): result[next(iter(name))]
+            for name in result.names()
+        }
