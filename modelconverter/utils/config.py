f22ea1ea180069b2629e6828db38f87d2cb0ba54
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-10-30 13:33:29 +0100

    changed per_row_quantization to false

--- modelconverter/utils/config.py
+++ modelconverter/utils/config.py
@@ -330,7 +330,7 @@ class RVC4Config(TargetConfig):
     snpe_dlc_graph_prepare_args: List[str] = []
     keep_raw_images: bool = False
     use_per_channel_quantization: bool = True
-    use_per_row_quantization: bool = True
+    use_per_row_quantization: bool = False
     htp_socs: List[
         Literal["sm8350", "sm8450", "sm8550", "sm8650", "qcs6490", "qcs8550"]
     ] = ["sm8550"]
