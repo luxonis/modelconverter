4536974c3fbdd49483014cfd4c030bad9a2e905d
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-03-06 01:12:29 +0100

    remove disable onnx opt

--- modelconverter/hub/__main__.py
+++ modelconverter/hub/__main__.py
@@ -709,8 +709,6 @@ def convert(
         upload(str(cfg.input_model), instance_id)
 
     target_options = get_target_specific_options(target, cfg, tool_version)
-    if "disable_onnx_optimization" in target_options:
-        del target_options["disable_onnx_optimization"]
     instance = _export(
         f"{variant_name} exported to {target}",
         instance_id,
