cf3592601f93256d3ee0f1e6555b838a318dd28a
Petros Toupas <ptoupas@gmail.com>
2025-06-20 16:55:32 +0300

    ONNX Split node compatibility (#122)

--- modelconverter/utils/onnx_tools.py
+++ modelconverter/utils/onnx_tools.py
@@ -7,12 +7,19 @@ import onnx
 import onnx_graphsurgeon as gs
 import onnxruntime as ort
 from loguru import logger
-from onnx import checker, helper
+from onnx import TensorProto, checker, helper
 from onnxsim import simplify
 
 from modelconverter.utils.config import InputConfig
 
 from .exceptions import ONNXException
+
+
+def get_opset_version(model: onnx.ModelProto) -> int:
+    for imp in model.opset_import:
+        if imp.domain == "":
+            return imp.version
+    raise ONNXException("No opset version found in the ONNX model.")
 
 
 def onnx_attach_normalization_to_inputs(
@@ -78,21 +85,42 @@ def onnx_attach_normalization_to_inputs(
 
         # 1. Reverse channels if needed
         if cfg.encoding_mismatch:
+            opset = get_opset_version(model)
             split_names = [f"split_{i}_{input_name}" for i in range(3)]
-            split_node = helper.make_node(
-                "Split",
-                inputs=[last_output],
-                outputs=split_names,
-                axis=1 if layout == "NCHW" else 3,
-                name=f"split_{input_name}",
-            )
+            axis = 1 if layout == "NCHW" else 3
+
+            if opset < 13:
+                split_node = helper.make_node(
+                    "Split",
+                    inputs=[last_output],
+                    outputs=split_names,
+                    axis=axis,
+                    split=[1] * n_channels,
+                    name=f"split_{input_name}",
+                )
+            else:
+                split_lengths_name = f"split_{input_name}_lengths"
+                split_lengths_tensor = helper.make_tensor(
+                    name=split_lengths_name,
+                    data_type=TensorProto.INT64,
+                    dims=[n_channels],
+                    vals=[1] * n_channels,
+                )
+                new_initializers.append(split_lengths_tensor)
+                split_node = helper.make_node(
+                    "Split",
+                    inputs=[last_output, split_lengths_name],
+                    outputs=split_names,
+                    axis=axis,
+                    name=f"split_{input_name}",
+                )
             new_nodes.append(split_node)
 
             concat_node = helper.make_node(
                 "Concat",
                 inputs=split_names[::-1],
                 outputs=[f"normalized_{input_name}"],
-                axis=1 if layout == "NCHW" else 3,
+                axis=axis,
                 name=f"concat_{input_name}",
             )
             new_nodes.append(concat_node)
