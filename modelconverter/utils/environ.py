acb5eb5cc6680be3ecc455a44da366649cfaa071
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-07-23 14:04:33 +0200

    removed keyring

--- modelconverter/utils/environ.py
+++ modelconverter/utils/environ.py
@@ -1,10 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
-from contextlib import suppress
 
 import keyring
 from luxonis_ml.utils import Environ as BaseEnviron
-from pydantic import model_validator
-from typing_extensions import Self
 
 
 def get_password_with_timeout(
@@ -24,18 +21,6 @@ def get_password_with_timeout(
 class Environ(BaseEnviron):
     HUBAI_API_KEY: str | None = None
     HUBAI_URL: str = "https://easyml.cloud.luxonis.com/"
-
-    @model_validator(mode="after")
-    def validate_hubai_api_key(self) -> Self:
-        if self.HUBAI_API_KEY:
-            return self
-
-        with suppress(Exception):
-            self.HUBAI_API_KEY = get_password_with_timeout(
-                "ModelConverter", "api_key"
-            )
-
-        return self
 
 
 environ = Environ()
