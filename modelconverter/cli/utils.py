c09042862676f17010528a43f8e1b5f9687dbd19
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-08-19 11:49:39 +0200

    n shaves

--- modelconverter/cli/utils.py
+++ modelconverter/cli/utils.py
@@ -434,6 +434,7 @@ def get_target_specific_options(
             options["pot_target_device"] = cfg.rvc3.pot_target_device
         if target is Target.RVC2:
             options["superblob"] = cfg.rvc2.superblob
+            options["number_of_shaves"] = cfg.rvc2.number_of_shaves
     elif target is Target.HAILO:
         options["optimization_level"] = cfg.hailo.optimization_level
         options["compression_level"] = cfg.hailo.compression_level
