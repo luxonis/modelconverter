82b5d7f035a50993546d71cf840f238a2fd4bdf3
klemen1999 <klemenskrlj8@gmail.com>
2026-06-23 23:04:48 +0200

    Release v0.5.6

--- modelconverter/__init__.py
+++ modelconverter/__init__.py
@@ -4,7 +4,7 @@ from typing import Final
 from luxonis_ml.utils import PUT_FILE_REGISTRY
 from pydantic_extra_types.semantic_version import SemanticVersion
 
-__version__: Final[str] = "0.5.5"
+__version__: Final[str] = "0.5.6"
 __semver__: Final[SemanticVersion] = SemanticVersion.parse(__version__)
 
 
